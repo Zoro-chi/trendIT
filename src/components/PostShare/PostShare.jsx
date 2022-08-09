511efedd7bc7c33e0e2d1026b58ef95264418f07
zoro <jagbetuyi001@gmail.com>
2022-08-09 04:29:53 +0100

    client-side post

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,36 +1,68 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { HiOutlinePhotograph } from "react-icons/hi";
 import { MdSlowMotionVideo } from "react-icons/md";
 import { MdOutlineLocationOn } from "react-icons/md";
 import { GoCalendar } from "react-icons/go";
+import { FaTimes } from "react-icons/fa";
 
 import "./PostShare.css";
 import profilepic from "../../images/profilepic.jpg";
 
 function PostShare() {
+  const [image, setImage] = useState(null);
+  const imageRef = useRef();
+
+  const onImageChange = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      let img = e.target.files[0];
+      setImage({
+        image: URL.createObjectURL(img),
+      });
+    }
+  };
+
   return (
     <div className="postshare">
       <img src={profilepic} alt="" />
       <div>
         <input type="text" placeholder="What's up?" />
-      </div>
-      <div className="postOptions">
-        <div className="option">
-          <HiOutlinePhotograph />
-          Photo
+        <div className="postOptions">
+          <div
+            className="option"
+            style={{ color: "dodgerBlue" }}
+            onClick={() => imageRef.current.click()}
+          >
+            <HiOutlinePhotograph style={{ height: "20px", width: "20px" }} />
+            Photo
+          </div>
+          <div className="option" style={{ color: "darkMagenta" }}>
+            <MdSlowMotionVideo style={{ height: "20px", width: "20px" }} />
+            Video
+          </div>
+          <div className="option" style={{ color: "green" }}>
+            <MdOutlineLocationOn style={{ height: "20px", width: "20px" }} />
+            Location
+          </div>
+          <div className="option" style={{ color: "darkGoldenrod" }}>
+            <GoCalendar style={{ height: "20px", width: "20px" }} />
+            Schedule
+          </div>
+          <button className="button psButton">Share</button>
+          <div style={{ display: "none" }}>
+            <input
+              type="file"
+              name="myImage"
+              ref={imageRef}
+              onChange={onImageChange}
+            />
+          </div>
         </div>
-        <div className="option">
-          <MdSlowMotionVideo />
-          Video
-        </div>
-        <div className="option">
-          <MdOutlineLocationOn />
-          Location
-        </div>
-        <div className="option">
-          <GoCalendar />
-          Schedule
-        </div>
+        {image && (
+          <div className="previewImage">
+            <FaTimes onClick={() => setImage(null)} />
+            <img src={image.image} alt="" />
+          </div>
+        )}
       </div>
     </div>
   );
