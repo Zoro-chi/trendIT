fcda6e4f132649485a5f6e07441c08d699404ea7
zoro <jagbetuyi001@gmail.com>
2022-08-11 07:36:17 +0100

    heroku

--- server/index.js
+++ server/index.js
@@ -26,12 +26,23 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(process.env.PORT || PORT, () => console.log("Listening"))
+    app.listen(process.env.PORT || 5000, () => console.log("Listening"))
   )
   .catch((error) => console.log(error));
 
 //   USAGE OF ROUTES
-app.use("/auth", AuthRoute);
-app.use("/user", UserRoute);
-app.use("/post", PostRoute);
-app.use("/upload", uploadRoute);
+app.use("/api/auth", AuthRoute);
+app.use("/api/user", UserRoute);
+app.use("/api/post", PostRoute);
+app.use("/api/upload", uploadRoute);
+
+// HEROKU DEPLOY
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static("client-side/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client-side/build/index.html"));
+  });
+}
