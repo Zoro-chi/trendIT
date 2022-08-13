62eb6b3ba62b16bd32e26d2ed10a56573abab27d
zoro <jagbetuyi001@gmail.com>
2022-08-13 17:30:38 +0100

    heroku

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+dotenv.config();
 
 import AuthRoute from "./Routes/AuthRoute.js";
 import UserRoute from "./Routes/UserRoute.js";
@@ -18,17 +19,33 @@ app.use("/images", express.static("images"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO_DB_URL || process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(process.env.PORT || 5000, () => console.log("Listening"))
-  )
-  .catch((error) => console.log(error));
+// mongoose
+//   .connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() =>
+//     mongoose.connection.once("open", () => {
+//       console.log("Conection has been made!");
+//     })
+//   )
+//   .catch((error) => console.log(error));
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(String(process.env.MONGO_URI), {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log(`mongo database is connected!!! ${conn.connection.host} `);
+  } catch (error) {
+    console.error(`Error: ${error} `);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 //   USAGE OF ROUTES
 app.use("/api/auth", AuthRoute);
@@ -46,3 +63,5 @@ app.use("/api/upload", uploadRoute);
 //     res.sendFile(path.join(__dirname + "/client-side/build/index.html"));
 //   });
 // }
+
+app.listen(process.env.PORT || 5000, () => console.log("Listening"));
