c1fd02c4078675794c6b48c76bff97b58926f0f5
zoro <jagbetuyi001@gmail.com>
2022-08-13 14:55:06 +0100

    heroku

--- client-side/src/Api/authRequest.js
+++ client-side/src/Api/authRequest.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "http://localhost:5000" || process.env.PORT,
+});
 
 export const logIn = (formData) => API.post("/api/auth/login", formData);
 export const signUp = (formData) => API.post("/api/auth/register", formData);
