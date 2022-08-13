c9b535e97f58cf436926d7512eb98860f744750e
zoro <jagbetuyi001@gmail.com>
2022-08-13 16:30:26 +0100

    heroku

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cors());
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(process.env.MONGODB_URI || process.env.MONGO_DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
