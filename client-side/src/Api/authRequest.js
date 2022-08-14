ec5f5c50230c89ad126bb4d175c2cb51ded41ddc
zoro <jagbetuyi001@gmail.com>
2022-08-14 22:37:07 +0100

    build fix

--- client-side/src/Api/authRequest.js
+++ client-side/src/Api/authRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://trenddit.herokuapp.com/" || "http://localhost:5000",
 });
 
 export const logIn = (formData) => API.post("/api/auth/login", formData);
